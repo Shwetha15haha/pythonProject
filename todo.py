8f211ab626cf85d7ed0823de05ec1586d6ddbbcb
moghe <mogheshwetha15@gmail.com>
2024-09-18 20:31:03 +0530

    structure and description

--- todo.py
+++ todo.py
@@ -12,3 +12,14 @@ while True:
         case "exit":
             break
 print("Bye!")
+
+# The match statement in Python is a control flow construct introduced in Python 3.10. It allows for pattern matching, which is similar to a switch-case statement in other languages but more powerful.
+# In your example, the match statement is used to compare the value of user_action against different cases. Depending on the value, a corresponding block of code is executed.
+
+# match variable:
+#     case "value1":
+#         # do something
+#     case "value2":
+#         # do something else
+#     case _:
+#         # default case if no match
