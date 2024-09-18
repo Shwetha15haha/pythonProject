0e3b08bb5af38f41dd38ea9b29cfc750452edc77
moghe <mogheshwetha15@gmail.com>
2024-09-18 21:45:14 +0530

    exercise codes

--- exercise.py
+++ exercise.py
@@ -1,3 +1,4 @@
+1.
 name = input("Enter your name: ")
 
 while True:
@@ -6,3 +7,25 @@ while True:
 while True:
     name = input("Enter your name: ")
     print(name.capitalize())
+
+2.
+members = ["john smith", "sen plakay", "dora ngacely"]
+
+for member in members:
+    print(member.title())
+
+3.
+country = "India"
+match country:
+    case "USA":
+        print("Hello")
+    case "India":
+        print("Namaste")
+    case "Germany":
+        print("Hallo")
+
+4.
+colors = [11, 34, 98, 43, 45, 54, 54]
+
+for color in colors:
+    print(color)
