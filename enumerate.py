abe91823f5c5dd30735f9dff3eee37d2fea62ae8
moghe <mogheshwetha15@gmail.com>
2024-09-22 19:06:20 +0530

    more exercise codes

--- enumerate.py
+++ enumerate.py
@@ -1,10 +1,14 @@
 for i,j in enumerate("Shwetha"):
     print(f"{i+1}.{j}")
-print("")
+# -----------------------------------------------------------------------
 for i,j in enumerate([["a", "b"], ["c", "d"]]):
     print(f"{i+1}.{j}")
-print("")
+# -----------------------------------------------------------------------
 waiting_list = ["john", "ben", "sen"]
 waiting_list.sort()
 for i, j in enumerate(waiting_list):
     print(f"{i+1}.{j.capitalize()}")
+# --------------------------------------------------------------------------------
+buttons = [('John', 'Sen', 'Morro'), ('Lin', 'Ajay', 'Filip')]
+for first, second, third in buttons:
+    print(first, second, third)
