1bcb3d81cfb2caedae54819daef3e0430411ba38
moghe <mogheshwetha15@gmail.com>
2024-09-22 18:28:41 +0530

    more exercise codes

--- exercise.py
+++ exercise.py
@@ -1,57 +1,57 @@
-# 1.
-# name = input("Enter your name: ")
-#
-# while True:
-#     print(name.capitalize())
-#
-# while True:
-#     name = input("Enter your name: ")
-#     print(name.capitalize())
-#
-# 2.
-# members = ["john smith", "sen play", "dora nacelle"]
-#
-# for member in members:
-#     print(member.title())
-#
-# 3.
-# country = "India"
-# match country:
-#     case "USA":
-#         print("Hello")
-#     case "India":
-#         print("Namaste")
-#     case "Germany":
-#         print("Hallo")
-#
-# 4.
-# colors = [11, 34, 98, 43, 45, 54, 54]
-#
-# for color in colors:
-#     print(color)
+1.
+name = input("Enter your name: ")
 
-# 5.
-# dollars = float(input("Enter the amount in dollars :"))
-#
-# rate = 2
-#
-# euros = dollars * rate
-#
-# print("The amount in euros:", euros)
+while True:
+    print(name.capitalize())
 
-# 6.
-# ranking = ['John', 'Sen', 'Lisa']
+while True:
+    name = input("Enter your name: ")
+    print(name.capitalize())
 
-# for r in ranking:
-#     rank = ranking.index(r)
-#     rank = int(input("Enter rank number:"))
-#     rank = rank -1
-#     print(ranking[rank])
-#     break
+2.
+members = ["john smith", "sen play", "dora nacelle"]
 
-# rank = int(input("Enter a rank number:"))-1
-# name = ranking[rank]
-# print(name)
+for member in members:
+    print(member.title())
+
+3.
+country = "India"
+match country:
+    case "USA":
+        print("Hello")
+    case "India":
+        print("Namaste")
+    case "Germany":
+        print("Hallo")
+
+4.
+colors = [11, 34, 98, 43, 45, 54, 54]
+
+for color in colors:
+    print(color)
+
+5.
+dollars = float(input("Enter the amount in dollars :"))
+
+rate = 2
+
+euros = dollars * rate
+
+print("The amount in euros:", euros)
+
+6.
+ranking = ['John', 'Sen', 'Lisa']
+
+for r in ranking:
+    rank = ranking.index(r)
+    rank = int(input("Enter rank number:"))
+    rank = rank -1
+    print(ranking[rank])
+    break
+
+rank = int(input("Enter a rank number:"))-1
+name = ranking[rank]
+print(name)
 
 name = input("Enter a name: ")
 
@@ -59,5 +59,16 @@ rank = ranking.index(name)+1
 
 print(rank)
 
+7.
+filenames = ['document', 'report', 'presentation']
 
+for index, filename in enumerate(filenames):
+    print(f"{index}-{filename.capitalize()}.txt")
+
+8.
+ips = ['100.122.133.105', '100.122.133.111']
+
+index = int(input("Enter the index of the IP you want: "))
+
+print("You chose",ips[index])
 
