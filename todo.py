654c6c6126f31eb6b0cf1fb8859d7cabe46b7260
moghe <mogheshwetha15@gmail.com>
2024-09-22 17:45:32 +0530

    added code to remove todo when the task is completed

--- todo.py
+++ todo.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit: ").lower().strip()
+    user_action = input("Type add, show, edit, exit or complete: ").lower().strip()
 
     match user_action:
         case "add":
@@ -12,19 +12,22 @@ while True:
             for index, todo in enumerate(todos, 1):
                 print(f"{index}. {todo}")
         case "edit":
-            number = int(input("Enter the number of todo: "))
-            number = number - 1
+            number = int(input("Enter the number of todo to edit: "))-1
             new_todo = input("Enter a new to do: ")
-            todos[number] = new_todo
-            print(new_todo.title())
+            todos[number] = new_todo.title()
+        case "complete":
+            number = int(input("Enter the number of todo to complete: "))-1
+            todos.pop(number)
         case "exit":
             break
         case _:
             print("Unknown command. Please type add, show, or exit.")
 print("Bye!")
 
-# The match statement in Python is a control flow construct introduced in Python 3.10. It allows for pattern matching, which is similar to a switch-case statement in other languages but more powerful.
-# In your example, the match statement is used to compare the value of user_action against different cases. Depending on the value, a corresponding block of code is executed.
+# The match statement in Python is a control flow construct introduced in Python 3.10.
+# It allows for pattern matching, which is similar to a switch-case statement in other languages but more powerful.
+# In your example, the match statement is used to compare the value of user_action against different cases.
+# Depending on the value, a corresponding block of code is executed.
 
 # match variable:
 #     case "value1":
