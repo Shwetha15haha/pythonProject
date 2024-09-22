2d64fb1b1d671a2b0ac35bf1da6a6679eb703db6
moghe <mogheshwetha15@gmail.com>
2024-09-22 18:13:04 +0530

    more example, sorting the list

--- enumerate.py
+++ enumerate.py
@@ -1,4 +1,10 @@
 for i,j in enumerate("Shwetha"):
-    print(f"{i}.{j}")
+    print(f"{i+1}.{j}")
+print("")
 for i,j in enumerate([["a", "b"], ["c", "d"]]):
-    print(f"{i}.{j}")
+    print(f"{i+1}.{j}")
+print("")
+waiting_list = ["john", "ben", "sen"]
+waiting_list.sort()
+for i, j in enumerate(waiting_list):
+    print(f"{i+1}.{j.capitalize()}")
