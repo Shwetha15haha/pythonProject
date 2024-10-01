0b0ff5caf415d68788d642efa44bdc808c0c4074
moghe <mogheshwetha15@gmail.com>
2024-10-01 19:13:04 +0530

    more user friendly prompt and if/elif statement usage

--- todo.py
+++ todo.py
@@ -1,135 +1,132 @@
 while True:
-    # Prompt the user to enter an action and convert it to lowercase and strip any extra spaces
-    user_action = input("Type add, show, edit, exit or complete: ").lower().strip()
+    # Prompt the user to enter an action, convert it to lowercase, and strip any extra spaces
+    user_action = input("Type add, show, edit, complete or exit: ").lower().strip()
 
-    # Match the user action to the corresponding case
-    match user_action:
-        case "add":
-            # Prompt the user to enter a new todo item
-            todo = input("Enter a todo: ") + "\n"
+    # Check if the user action is "add"
+    if "add" in user_action:
+        # Extract the todo from user input
+        todo = user_action[4:] + "\n"
 
-            # Open the file in read mode and read all lines
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
+        # Open the file in read mode and read all lines
+        with open('files/todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            # Append the new todo item to the list, capitalizing the first letter of each word
-            todos.append(todo.title())
+        # Append the new todo item to the list, capitalizing the first letter of each word
+        todos.append(todo.title())
 
-            # Open the file in write mode and write all todos back to the file
-            with open('files/todos.txt', 'w') as file:
-                file.writelines(todos)
+        # Open the file in write mode and write all todos back to the file
+        with open('files/todos.txt', 'w') as file:
+            file.writelines(todos)
 
-        case "show":
-            # Open the file in read mode and read all lines
-            print('Your todos list:')
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
+    # Check if the user action is "show"
+    elif "show" in user_action:
+        # Open the file in read mode and read all lines
+        print('Your todos list:')
+        with open('files/todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            # Enumerate through the todos and print each one with its index
-            for index, todo in enumerate(todos, 1):
-                todo = todo.strip('\n')  # Remove the newline character
-                print(f"{index}. {todo}")
+        # Enumerate through the todos and print each one with its index
+        for index, todo in enumerate(todos, 1):
+            todo = todo.strip('\n')  # Remove the newline character
+            print(f"{index}. {todo}")
 
-        case "edit":
-            # Prompt the user to enter the number of the todo item to edit
-            number = int(input("Enter the number of todo to edit: ")) - 1
+    # Check if the user action is "edit"
+    elif "edit" in user_action:
+        # Extract the number of the todo item to edit
+        number = int(user_action[5:])-1
 
-            # Open the file in read mode and read all lines
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
+        # Open the file in read mode and read all lines
+        with open('files/todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            # Display the existing todos
-            print('Here are the existing todos:', todos)
+        # Display the existing todos
+        print('Here are the existing todos:', todos)
 
-            # Prompt the user to enter a new todo item
-            new_todo = input("Enter a new todo: ")
+        # Prompt the user to enter a new todo item
+        new_todo = input("Enter a new todo: ")
 
-            # Update the specified todo item
-            todos[number] = new_todo.title() + '\n'
+        # Update the specified todo item
+        todos[number] = new_todo.title() + '\n'
 
-            # Display the updated todos
-            print('Here are the updated todos:', todos)
+        # Display the updated todos
+        print('Here are the updated todos:', todos)
 
-            # Open the file in write mode and write all todos back to the file
-            with open('files/todos.txt', 'w') as file:
-                file.writelines(todos)
+        # Open the file in write mode and write all todos back to the file
+        with open('files/todos.txt', 'w') as file:
+            file.writelines(todos)
 
-        case "complete":
-            # Prompt the user to enter the number of the todo item to complete
-            number = int(input("Enter the number of todo to complete: ")) - 1
+    # Check if the user action is "complete"
+    elif "complete" in user_action:
+        # Extract the number of todo item to remove
+        number = int(user_action[9:]) - 1
 
-            # Open the file in read mode and read all lines
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
-            todo_to_remove = todos[number].strip()
-            # Remove the specified todo item from the list
-            todos.pop(number)
+        # Open the file in read mode and read all lines
+        with open('files/todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            # Open the file in write mode and write the remaining todos back to the file
-            with open('files/todos.txt', 'w') as file:
-                file.writelines(todos)
-            # Printing message to user about removed todo
-            message = f"Todo {todo_to_remove} is removed from todo list"
-            print(message)
+        # Remove the specified todo item from the list
+        todo_to_remove = todos.pop(number).strip()
 
-        case "exit":
-            # Exit the loop
-            break
+        # Open the file in write mode and write the remaining todos back to the file
+        with open('files/todos.txt', 'w') as file:
+            file.writelines(todos)
 
-        case _:
-            # Handle unknown commands
-            print("Unknown command. Please type add, show, edit, complete, or exit.")
+        # Print a message to the user about the removed todo
+        print(f"Todo '{todo_to_remove}' is removed from the todo list")
+
+    # Check if the user action is "exit"
+    elif "exit" in user_action:
+        # Exit the loop
+        break
+
+    # Handle unknown commands
+    else:
+        print('Unknown command. Please type add, show, edit, complete, or exit.')
 
 print("Bye!")
 
 # i/o :
-# Type add, show, edit, exit or complete: add
-# Enter a todo: Eat chocolate
-# Type add, show, edit, exit or complete: show
+# Type add, show, edit, complete or exit: add
+# Type add, show, edit, complete or exit: show
 # Your todos list:
-# 1. Eat Healthy
-# 2. Sleep
-# 3. Code
-# 4. Debug
-# 5. Chill , Just Relax
-# 6. Eat Chocolate
-# Type add, show, edit, exit or complete: edit
-# Enter the number of todo to edit: 5
-# Here are the existing todos: ['Eat Healthy\n', 'Sleep\n', 'Code\n', 'Debug\n', 'Chill , Just Relax\n', 'Eat Chocolate\n']
-# Enter a new todo: chill!just relax buddy
-# Here are the updated todos: ['Eat Healthy\n', 'Sleep\n', 'Code\n', 'Debug\n', 'Chill!Just Relax Buddy\n', 'Eat Chocolate\n']
-# Type add, show, edit, exit or complete: show
-# Your todos list:
-# 1. Eat Healthy
-# 2. Sleep
-# 3. Code
-# 4. Debug
-# 5. Chill!Just Relax Buddy
-# 6. Eat Chocolate
-# Type add, show, edit, exit or complete: complete
-# Enter the number of todo to complete: 1
-# Todo Eat Healthy is removed from todo list
-# Type add, show, edit, exit or complete: show
-# Your todos list:
-# 1. Sleep
+# 1. Sleep On Time
 # 2. Code
 # 3. Debug
 # 4. Chill!Just Relax Buddy
 # 5. Eat Chocolate
-# Type add, show, edit, exit or complete: exit
+# 6. Tea
+# 7. Read
+# 8. Buy Groceries
+# 9. Holiday Plan
+# 10.
+# Type add, show, edit, complete or exit: edit 10
+# Here are the existing todos: ['Sleep On Time\n', 'Code\n', 'Debug\n', 'Chill!Just Relax Buddy\n', 'Eat Chocolate\n', 'Tea\n', 'Read\n', 'Buy Groceries\n', 'Holiday Plan\n', '\n']
+# Enter a new todo: Watch movie
+# Here are the updated todos: ['Sleep On Time\n', 'Code\n', 'Debug\n', 'Chill!Just Relax Buddy\n', 'Eat Chocolate\n', 'Tea\n', 'Read\n', 'Buy Groceries\n', 'Holiday Plan\n', 'Watch Movie\n']
+# Type add, show, edit, complete or exit: show
+# Your todos list:
+# 1. Sleep On Time
+# 2. Code
+# 3. Debug
+# 4. Chill!Just Relax Buddy
+# 5. Eat Chocolate
+# 6. Tea
+# 7. Read
+# 8. Buy Groceries
+# 9. Holiday Plan
+# 10. Watch Movie
+# Type add, show, edit, complete or exit: complete 6
+# Todo 'Tea' is removed from the todo list
+# Type add, show, edit, complete or exit: show
+# Your todos list:
+# 1. Sleep On Time
+# 2. Code
+# 3. Debug
+# 4. Chill!Just Relax Buddy
+# 5. Eat Chocolate
+# 6. Read
+# 7. Buy Groceries
+# 8. Holiday Plan
+# 9. Watch Movie
+# Type add, show, edit, complete or exit: exit
 # Bye!
-
-
-
-# The match statement in Python is a control flow construct introduced in Python 3.10.
-# It allows for pattern matching, which is similar to a switch-case statement in other languages but more powerful.
-# In your example, the match statement is used to compare the value of user_action against different cases.
-# Depending on the value, a corresponding block of code is executed.
-
-# match variable:
-#     case "value1":
-#         # do something
-#     case "value2":
-#         # do something else
-#     case _:
-#         # default case if no match
