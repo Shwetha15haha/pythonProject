c616c1f5d0158282be733f3d3e4c782adee1e122
moghe <mogheshwetha15@gmail.com>
2024-10-01 20:31:30 +0530

    new exercise code checking password is strong or weak

--- password.py
+++ password.py
@@ -2,9 +2,9 @@
 # until the correct password "pass123" is entered
 password = input("Enter the password: ")
 
-while password != "pass123":
-    password = input("Enter the password: ")
-print("Password is correct")
+# while password != "pass123":
+#     password = input("Enter the password: ")
+# print("Password is correct")
 
 # input :
 # Enter the password: 123
@@ -12,3 +12,28 @@ print("Password is correct")
 # Enter the password: pass123
 # output :
 # Password is correct
+
+result =[]
+if len(password) >= 8:
+    result.append(True)
+else:
+    result.append(False)
+
+digit = False
+for i in password:
+    if i.isdigit():
+        digit = True
+result.append(digit)
+
+upper = False
+for i in password:
+    if i.isupper():
+        upper = True
+result.append(upper)
+
+print(result)
+
+if all(result):
+    print('Strong Password')
+else:
+    print('Weak Password')
