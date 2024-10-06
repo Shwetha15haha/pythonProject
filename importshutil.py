8368b870455027db9773d48a398db0af8edd80d7
moghe <mogheshwetha15@gmail.com>
2024-10-06 19:35:50 +0530

    use of shutil module

---
+++ importshutil.py
@@ -0,0 +1,10 @@
+"""
+This module demonstrates how to use the shutil module to create a zip archive.
+
+The shutil module offers a number of high-level operations on files and collections of files. In particular, functions are provided which support file copying and removal.
+"""
+
+import shutil  # Import the shutil module for high-level file operations
+
+# Create a ZIP archive from the 'files' directory, and name it 'output.zip'
+shutil.make_archive('files/weather.csv', 'zip', 'files')
