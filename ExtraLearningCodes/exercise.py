2476da4c6b6bff053228ba898358f9b8842cb82a
moghe <mogheshwetha15@gmail.com>
2024-10-07 18:24:42 +0530

    new exercises added on GUI

--- ExtraLearningCodes/exercise.py
+++ ExtraLearningCodes/exercise.py
@@ -464,3 +464,44 @@ window.read()
 
 # Close the window when the interaction is complete
 window.close()
+
+
+# 25.
+#
+# import FreeSimpleGUI as sg
+#
+# label = sg.Text("What are dolphins?")
+# option1 = sg.Radio("Amphibians", group_id="question1")
+# option2 = sg.Radio("Fish", group_id="question1")
+# option3 = sg.Radio("Mammals", group_id="question1")
+# option4 = sg.Radio("Birds", group_id="question1")
+#
+# window = sg.Window("Quiz",
+#                    layout=[[label],
+#                            [option1,
+#                             option2,
+#                             option3,
+#                             option4],
+#                            ])
+#
+# window.read()
+# window.close()
+###############################################################################################
+# import FreeSimpleGUI as sg
+#
+# label = sg.Text("What are dolphins?")
+# option1 = sg.Radio("Amphibians", group_id="question1")
+# option2 = sg.Radio("Fish", group_id="question1")
+# option3 = sg.Radio("Mammals", group_id="question1")
+# option4 = sg.Radio("Birds", group_id="question1")
+#
+# window = sg.Window("Quiz",
+#                    layout=[[label],
+#                            [option1],
+#                            [option2],
+#                            [option3],
+#                            [option4],
+#                            ])
+#
+# window.read()
+# window.close()
