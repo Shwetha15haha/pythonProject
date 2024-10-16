60666a3db05a5ec11db8c7529626355cabd9fccf
moghe <mogheshwetha15@gmail.com>
2024-10-16 21:10:24 +0530

    creating a web app

--- TododAPP/webapp.py
+++ TododAPP/webapp.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
+import functions
+
 st.title('Todo App')
 st.subheader('For personal use')
 st.write('Jotdown everything you need to do now')
+
+todos = functions.get_todos()
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label='', placeholder='Enter to do....')
+
 
