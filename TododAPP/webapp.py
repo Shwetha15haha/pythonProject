46f80e1fdc91aafc916d06dfc8e1421e74c8b9c9
moghe <mogheshwetha15@gmail.com>
2024-10-19 19:44:42 +0530

    added new todo button and appending to the todo list

--- TododAPP/webapp.py
+++ TododAPP/webapp.py
@@ -2,15 +2,21 @@ import streamlit as st
 
 import functions
 
+todos = functions.get_todos()
+
+
+def add_todo():
+    todo = st.session_state['new_todo']
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title('Todo App')
 st.subheader('For personal use')
-st.write('Jotdown everything you need to do now')
+st.write('Jot down everything you need to do now')
 
-todos = functions.get_todos()
 
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label='', placeholder='Enter to do....')
-
-
+st.text_input(label='', placeholder='Enter to do....', on_change=add_todo, key='new_todo')
