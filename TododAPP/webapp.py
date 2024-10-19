f2752cacb42f9651123d46092b0d461c5e1aa5ae
moghe <mogheshwetha15@gmail.com>
2024-10-19 20:04:01 +0530

    removing todo from list when its checked, updating enter do widget

--- TododAPP/webapp.py
+++ TododAPP/webapp.py
@@ -9,6 +9,7 @@ def add_todo():
     todo = st.session_state['new_todo']
     todos.append(todo)
     functions.write_todos(todos)
+    st.session_state['new_todo'] = ''
 
 
 st.title('Todo App')
@@ -16,7 +17,12 @@ st.subheader('For personal use')
 st.write('Jot down everything you need to do now')
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label='', placeholder='Enter to do....', on_change=add_todo, key='new_todo')
