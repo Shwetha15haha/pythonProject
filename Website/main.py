a5875d4641c42b701f30b183aa84645ed7903a5f
moghe <mogheshwetha15@gmail.com>
2024-11-01 20:16:49 +0530

    added all app details in columns along with source code

--- Website/main.py
+++ Website/main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 col1, col2 = st.columns(2)
 
@@ -30,126 +31,26 @@ content2 = """
 Below you can find details of some basic Python apps built by me"""
 st.info(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
+df = pd.read_csv('data.csv', sep=';')
 with col3:
-    st.title('App1')
-    st.write('Descrition')
-    st.image('images/1.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
-
-    st.title('App3')
-    st.write('Descrition')
-    st.image('images/3.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
-
-    st.title('App5')
-    st.write('Descrition')
-    st.image('images/5.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
-
-    st.title('App7')
-    st.write('Descrition')
-    st.image('images/7.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
-
-    st.title('App9')
-    st.write('Descrition')
-    st.image('images/9.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
-
-    st.title('App11')
-    st.write('Descrition')
-    st.image('images/11.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
-
-    st.title('App13')
-    st.write('Descrition')
-    st.image('images/13.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
-
-    st.title('App15')
-    st.write('Descrition')
-    st.image('images/15.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
-
-    st.title('App17')
-    st.write('Descrition')
-    st.image('images/17.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
-
-    st.title('App19')
-    st.write('Descrition')
-    st.image('images/19.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
+    for index, row in df[:11].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/'+row['image'])
+        # url1 = row['url']
+        # st.page_link(url1, label='Source Code')
+        # st.link_button('Source Code', url1)
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
-    st.title('App2')
-    st.write('Descrition')
-    st.image('images/2.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
+    for index, row in df[11:].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/'+row['image'])
+        # url2 = row['url']
+        # st.page_link(url2, label='Source Code')
+        # st.link_button('Source Code', url2)
+        st.write(f"[Source Code]({row['url']})")
 
-    st.title('App4')
-    st.write('Descrition')
-    st.image('images/4.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
-
-    st.title('App6')
-    st.write('Descrition')
-    st.image('images/6.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
-
-    st.title('App8')
-    st.write('Descrition')
-    st.image('images/8.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
-
-    st.title('App10')
-    st.write('Descrition')
-    st.image('images/10.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
-
-    st.title('App12')
-    st.write('Descrition')
-    st.image('images/12.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
-
-    st.title('App14')
-    st.write('Descrition')
-    st.image('images/14.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
-
-    st.title('App16')
-    st.write('Descrition')
-    st.image('images/16.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
-
-    st.title('App18')
-    st.write('Descrition')
-    st.image('images/18.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
-
-    st.title('App20')
-    st.write('Descrition')
-    st.image('images/20.png')
-    url = 'https://github.com/Shwetha15haha/pythonProject/blob/master/TodoWebApp/web.py'
-    st.link_button('App code', url, type="secondary")
