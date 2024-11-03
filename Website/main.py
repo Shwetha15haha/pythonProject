77ab719dd5cb1fa4fe05b561cf47b346b2407f52
moghe <mogheshwetha15@gmail.com>
2024-11-03 19:47:36 +0530

    Relevent comments added

--- Website/main.py
+++ Website/main.py
@@ -1,8 +1,11 @@
+# Import packages
 import streamlit as st
 import pandas as pd
 
+# Create layout of 2 columns for introduction
 col1, col2 = st.columns(2)
 
+# Add content to the first column
 with col1:
     st.image('images/photo.jpg')
     st.markdown(
@@ -16,7 +19,7 @@ with col1:
         """,
         unsafe_allow_html=True
     )
-
+# Add content to the second column
 with col2:
     st.title('Shwetha Moghe')
     content = """
@@ -31,9 +34,13 @@ content2 = """
 Below you can find details of some basic Python apps built by me"""
 st.info(content2)
 
+# Create column layout for project details
 col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
+# Create dataframe using pandas
 df = pd.read_csv('data.csv', sep=';')
+
+# Add content to column by iterating over df
 with col3:
     for index, row in df[:11].iterrows():
         st.header(row['title'])
@@ -44,6 +51,7 @@ with col3:
         # st.link_button('Source Code', url1)
         st.write(f"[Source Code]({row['url']})")
 
+# Add content to column by iterating over df
 with col4:
     for index, row in df[11:].iterrows():
         st.header(row['title'])
@@ -53,4 +61,3 @@ with col4:
         # st.page_link(url2, label='Source Code')
         # st.link_button('Source Code', url2)
         st.write(f"[Source Code]({row['url']})")
-
