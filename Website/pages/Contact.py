d92b20824c2e4a233b2a2474adf33fe9458771c4
moghe <mogheshwetha15@gmail.com>
2024-11-04 19:53:26 +0530

    Creating mail message to contact

--- Website/pages/Contact.py
+++ Website/pages/Contact.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.header('Contact Me')
+st.header('Contact me for any feedback/message ')
+
+with st.form(key='email_forms'):
+    user_email = st.text_input(label='Please enter your email address below:', placeholder='Your email address')
+    message = st.text_area(label='Please share any query or feedback below:', placeholder='Your message')
+    button = st.form_submit_button()
+    if button:
+        print(button)
+        print("I was pressed")
