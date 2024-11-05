e0d0c7b399a71ffcc58f24efee7e2579c20e68d3
moghe <mogheshwetha15@gmail.com>
2024-11-05 20:14:39 +0530

    Creating mail message to contact

--- Website/Sending_Mail.py
+++ Website/Sending_Mail.py
@@ -1,23 +1,16 @@
 import smtplib
 import ssl
 
-host = 'smtp.gmail.com'
-port = 465
 
-username = 'mogheshwetha15@gmail.com'
-password = 'lgjw bzxr kfmm qrmv'
+def send_mail(message):
+    host = 'smtp.gmail.com'
+    port = 465
+    username = 'mogheshwetha15@gmail.com'
+    password = qrmv
+    receiver = 'mogheshwetha15@gmail.com'
 
-receiver = 'mogheshwetha15@gmail.com'
-
-message = """\
-Subject:Hi
-How are you?
-Bye.
-"""
-
-context = ssl.create_default_context()
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
--- Website/pages/Contact.py
+++ Website/pages/Contact.py
@@ -1,11 +1,18 @@
 import streamlit as st
+from Sending_Mail import send_mail
 
 st.header('Contact me for any feedback/message ')
 
 with st.form(key='email_forms'):
     user_email = st.text_input(label='Please enter your email address below:', placeholder='Your email address')
-    message = st.text_area(label='Please share any query or feedback below:', placeholder='Your message')
+    raw_message = st.text_area(label='Please share any query or feedback below:', placeholder='Your message')
+    message = f"""\
+Subject:New mail from {user_email}
+
+From : {user_email}
+{raw_message}
+"""
     button = st.form_submit_button()
     if button:
-        print(button)
-        print("I was pressed")
+        send_mail(message)
+        st.info('Your email was sent successfully.Thank you for contacting.')
